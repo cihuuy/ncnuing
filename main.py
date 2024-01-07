caaa5702ffac1a4d0ae5e34130181735a98dad68
cihuuy <95659029+cihuuy@users.noreply.github.com>
2024-01-07 10:34:29 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from colabcode import ColabCode
+from pyngrok import ngrok
+
+# Mengatur token ngrok
+ngrok.set_auth_token("2UMk1n8FdBsh6W8Y3RRpwAPObd3_7zieKW2iTcZCt2bMyMiA5")
+
+#atur koneksi
+ColabCode(port=10000, password="JUmbo555")
